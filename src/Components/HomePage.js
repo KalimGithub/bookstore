6ebd5cbc1fdebde05ab86179c60209308a274521
KalimGithub <sk22kalim@gmail.com>
2024-01-27 22:29:54 +0530

    added files

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react'
 
 const HomePage = () => {
     const [books, setBooks] = useState([]);
-    
+    const [book2, setBook2] = useState([]);
     useEffect(()=> {
         fetch('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
         .then(res => res.json())
         .then(data => {    
                 setBooks(data.items)
                 console.log(data.items)
+            }
+            )
+            .catch(error => console.log(error))
+        // https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes
+            fetch('https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes')
+            .then(res => res.json())
+            .then(data => {
+                setBook2(data.items)
+                console.log(data.items);
             }
             )
             .catch(error => console.log(error))
@@ -33,6 +42,19 @@ const HomePage = () => {
                     )
                 })
             }
+            </div>
+            <h1 id='more-book-text'>More Books</h1>
+            <div className='book-grid-div'>
+                {
+                    book2.map((book, index)=> {
+                        return(
+                            <div className='grid-book'>
+                                <img src={book.volumeInfo.imageLinks.thumbnail}/>
+                            </div>
+                        )
+                    })
+                }
+                <img />
             </div>
         </div>
     )
